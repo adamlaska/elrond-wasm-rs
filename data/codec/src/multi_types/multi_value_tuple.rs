ab91018fecff0002a87a2d331b79cbd9acfa41ed
Andrei Marinica <andrei.marinica@multiversx.com>
2026-02-23 03:31:36 +0200

    codec - multi value new

--- data/codec/src/multi_types/multi_value_tuple.rs
+++ data/codec/src/multi_types/multi_value_tuple.rs
@@ -29,7 +29,7 @@ macro_rules! multi_value_impls_no_debug {
 }
 
 macro_rules! multi_value_impls {
-    ($(($mv_struct:ident $len:tt $($n:tt $name:ident)+) )+) => {
+    ($(($mv_struct:ident $len:tt $($n:tt $name:ident $p:ident)+) )+) => {
         $(
             impl<$($name),+> From<($($name,)+)> for $mv_struct<$($name,)+> {
                 #[inline]
@@ -39,6 +39,11 @@ macro_rules! multi_value_impls {
             }
 
             impl<$($name,)+> $mv_struct<$($name,)+> {
+                #[inline]
+                pub fn new($($p: $name),+) -> Self {
+                    $mv_struct(($($p,)+))
+                }
+
                 #[inline]
                 pub fn into_tuple(self) -> ($($name,)+) {
                     self.0
@@ -132,21 +137,21 @@ multi_value_impls_no_debug! {
 }
 
 multi_value_impls! {
-    (MultiValue2   2 0 T0 1 T1)
-    (MultiValue3   3 0 T0 1 T1 2 T2)
-    (MultiValue4   4 0 T0 1 T1 2 T2 3 T3)
-    (MultiValue5   5 0 T0 1 T1 2 T2 3 T3 4 T4)
-    (MultiValue6   6 0 T0 1 T1 2 T2 3 T3 4 T4 5 T5)
-    (MultiValue7   7 0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6)
-    (MultiValue8   8 0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6 7 T7)
-    (MultiValue9   9 0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6 7 T7 8 T8)
-    (MultiValue10 10 0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6 7 T7 8 T8 9 T9)
-    (MultiValue11 11 0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6 7 T7 8 T8 9 T9 10 T10)
-    (MultiValue12 12 0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6 7 T7 8 T8 9 T9 10 T10 11 T11)
-    (MultiValue13 13 0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6 7 T7 8 T8 9 T9 10 T10 11 T11 12 T12)
-    (MultiValue14 14 0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6 7 T7 8 T8 9 T9 10 T10 11 T11 12 T12 13 T13)
-    (MultiValue15 15 0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6 7 T7 8 T8 9 T9 10 T10 11 T11 12 T12 13 T13 14 T14)
-    (MultiValue16 16 0 T0 1 T1 2 T2 3 T3 4 T4 5 T5 6 T6 7 T7 8 T8 9 T9 10 T10 11 T11 12 T12 13 T13 14 T14 15 T15)
+    (MultiValue2   2 0 T0 v0 1 T1 v1)
+    (MultiValue3   3 0 T0 v0 1 T1 v1 2 T2 v2)
+    (MultiValue4   4 0 T0 v0 1 T1 v1 2 T2 v2 3 T3 v3)
+    (MultiValue5   5 0 T0 v0 1 T1 v1 2 T2 v2 3 T3 v3 4 T4 v4)
+    (MultiValue6   6 0 T0 v0 1 T1 v1 2 T2 v2 3 T3 v3 4 T4 v4 5 T5 v5)
+    (MultiValue7   7 0 T0 v0 1 T1 v1 2 T2 v2 3 T3 v3 4 T4 v4 5 T5 v5 6 T6 v6)
+    (MultiValue8   8 0 T0 v0 1 T1 v1 2 T2 v2 3 T3 v3 4 T4 v4 5 T5 v5 6 T6 v6 7 T7 v7)
+    (MultiValue9   9 0 T0 v0 1 T1 v1 2 T2 v2 3 T3 v3 4 T4 v4 5 T5 v5 6 T6 v6 7 T7 v7 8 T8 v8)
+    (MultiValue10 10 0 T0 v0 1 T1 v1 2 T2 v2 3 T3 v3 4 T4 v4 5 T5 v5 6 T6 v6 7 T7 v7 8 T8 v8 9 T9 v9)
+    (MultiValue11 11 0 T0 v0 1 T1 v1 2 T2 v2 3 T3 v3 4 T4 v4 5 T5 v5 6 T6 v6 7 T7 v7 8 T8 v8 9 T9 v9 10 T10 v10)
+    (MultiValue12 12 0 T0 v0 1 T1 v1 2 T2 v2 3 T3 v3 4 T4 v4 5 T5 v5 6 T6 v6 7 T7 v7 8 T8 v8 9 T9 v9 10 T10 v10 11 T11 v11)
+    (MultiValue13 13 0 T0 v0 1 T1 v1 2 T2 v2 3 T3 v3 4 T4 v4 5 T5 v5 6 T6 v6 7 T7 v7 8 T8 v8 9 T9 v9 10 T10 v10 11 T11 v11 12 T12 v12)
+    (MultiValue14 14 0 T0 v0 1 T1 v1 2 T2 v2 3 T3 v3 4 T4 v4 5 T5 v5 6 T6 v6 7 T7 v7 8 T8 v8 9 T9 v9 10 T10 v10 11 T11 v11 12 T12 v12 13 T13 v13)
+    (MultiValue15 15 0 T0 v0 1 T1 v1 2 T2 v2 3 T3 v3 4 T4 v4 5 T5 v5 6 T6 v6 7 T7 v7 8 T8 v8 9 T9 v9 10 T10 v10 11 T11 v11 12 T12 v12 13 T13 v13 14 T14 v14)
+    (MultiValue16 16 0 T0 v0 1 T1 v1 2 T2 v2 3 T3 v3 4 T4 v4 5 T5 v5 6 T6 v6 7 T7 v7 8 T8 v8 9 T9 v9 10 T10 v10 11 T11 v11 12 T12 v12 13 T13 v13 14 T14 v14 15 T15 v15)
 }
 
 #[cfg(test)]
@@ -228,5 +233,30 @@ mod tests {
         let original = (0u8, 1u8, 2u8, 3u8, 4u8, 5u8, 6u8, 7u8, 8u8, 9u8, 10u8);
         let mv = MultiValue11::from(original);
         assert_eq!(mv.into_tuple(), original);
+    }
+
+    #[test]
+    fn multi_value2_new() {
+        let mv = MultiValue2::new(1u32, 2u64);
+        assert_eq!(mv.into_tuple(), (1u32, 2u64));
+    }
+
+    #[test]
+    fn multi_value3_new() {
+        let mv = MultiValue3::new(true, 42u8, 100u64);
+        assert_eq!(mv.into_tuple(), (true, 42u8, 100u64));
+    }
+
+    #[test]
+    fn multi_value5_new() {
+        let mv = MultiValue5::new(1u8, 2u16, 3u32, 4u64, false);
+        assert_eq!(mv.into_tuple(), (1u8, 2u16, 3u32, 4u64, false));
+    }
+
+    #[test]
+    fn multi_value2_new_eq_from() {
+        let from_tuple = MultiValue2::from((10u32, 20u32));
+        let from_new = MultiValue2::new(10u32, 20u32);
+        assert_eq!(from_tuple, from_new);
     }
 }
