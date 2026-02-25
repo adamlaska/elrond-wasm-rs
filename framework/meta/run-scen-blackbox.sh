82f760c0ecc4d73d32770c7279f55223f72a5dd0
Andrei Marinica <andrei.marinica@multiversx.com>
2026-02-25 18:03:35 +0200

    sh tool cleanup

--- framework/meta/run-scen-blackbox.sh
+++ framework/meta/run-scen-blackbox.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Run scen-blackbox for adder
-echo "Running scen-blackbox for adder..."
-cargo run scen-blackbox  --overwrite --path ../../contracts/examples/adder
-
 # Run scen-blackbox for digital-cash
-echo "Running scen-blackbox for digital-cash..."
-cargo run scen-blackbox --overwrite --path ../../contracts/examples/digital-cash
+echo "Running scen-blackbox for ping-pong-egld..."
+cargo run scen-blackbox --overwrite --path ../../contracts/examples/ping-pong-egld
 
 echo "Done!"
