584bf43c9a7d411d55433f5c118afe73e76cd1d1
Andrei Marinica <andrei.marinica@multiversx.com>
2026-02-25 18:15:07 +0200

    scen-blackbox - re-generated digital cash and ping-pong

--- contracts/examples/ping-pong-egld/tests/ping_pong_egld_blackbox_from_scenarios.rs
+++ contracts/examples/ping-pong-egld/tests/ping_pong_egld_blackbox_from_scenarios.rs
@@ -22,17 +22,39 @@ fn world() -> ScenarioWorld {
 }
 
 #[test]
-fn ping_pong_call_ping_before_beginning_scen() {
+fn ping_pong_call_get_user_addresses_scen() {
     let mut world = world();
-    ping_pong_call_ping_before_beginning_scen_steps(&mut world);
+    ping_pong_call_get_user_addresses_scen_steps(&mut world);
 }
 
-pub fn ping_pong_call_ping_before_beginning_scen_steps(world: &mut ScenarioWorld) {
+pub fn ping_pong_call_get_user_addresses_scen_steps(world: &mut ScenarioWorld) {
+    ping_pong_call_ping_second_user_scen_steps(world);
+
+    world
+        .tx()
+        .from(PARTICIPANT1_ADDRESS)
+        .to(PING_PONG_ADDRESS)
+        .typed(ping_pong_egld_proxy::PingPongEgldProxy)
+        .get_user_addresses()
+        .returns(ExpectValue(MultiValueVec::from(vec![
+            ScenarioValueRaw::new("address:participant1"),
+            ScenarioValueRaw::new("address:participant2"),
+        ])))
+        .run();
+}
+
+#[test]
+fn ping_pong_call_ping_after_deadline_scen() {
+    let mut world = world();
+    ping_pong_call_ping_after_deadline_scen_steps(&mut world);
+}
+
+pub fn ping_pong_call_ping_after_deadline_scen_steps(world: &mut ScenarioWorld) {
     ping_pong_init_scen_steps(world);
 
     world
         .current_block()
-        .block_timestamp_millis(TimestampMillis::new(779u64));
+        .block_timestamp_millis(TimestampMillis::new(123_781u64));
 
     world
         .tx()
@@ -41,33 +63,7 @@ pub fn ping_pong_call_ping_before_beginning_scen_steps(world: &mut ScenarioWorld
         .typed(ping_pong_egld_proxy::PingPongEgldProxy)
         .ping(IgnoreValue)
         .payment(Payment::try_new(TestTokenId::EGLD_000000, 0, 500_000_000_000u64).unwrap())
-        .with_result(ExpectError(4, "smart contract not active yet"))
-        .run();
-
-    world
-        .check_account(PING_PONG_ADDRESS)
-        .check_storage("str:activationTimestamp", "780")
-        .check_storage("str:deadline", "123,780")
-        .check_storage("str:pingAmount", "500,000,000,000");
-}
-
-#[test]
-fn ping_pong_call_ping_wrong_amount_scen() {
-    let mut world = world();
-    ping_pong_call_ping_wrong_amount_scen_steps(&mut world);
-}
-
-pub fn ping_pong_call_ping_wrong_amount_scen_steps(world: &mut ScenarioWorld) {
-    ping_pong_init_scen_steps(world);
-
-    world
-        .tx()
-        .from(PARTICIPANT1_ADDRESS)
-        .to(PING_PONG_ADDRESS)
-        .typed(ping_pong_egld_proxy::PingPongEgldProxy)
-        .ping(IgnoreValue)
-        .payment(Payment::try_new(TestTokenId::EGLD_000000, 0, 450_000_000_000u64).unwrap())
-        .with_result(ExpectError(4, "the payment must match the fixed sum"))
+        .with_result(ExpectError(4, "deadline has passed"))
         .run();
 
     world
@@ -108,33 +104,17 @@ pub fn ping_pong_call_ping_before_activation_scen_steps(world: &mut ScenarioWorl
 }
 
 #[test]
-fn ping_pong_call_pong_all_scen() {
+fn ping_pong_call_ping_before_beginning_scen() {
     let mut world = world();
-    ping_pong_call_pong_all_scen_steps(&mut world);
+    ping_pong_call_ping_before_beginning_scen_steps(&mut world);
 }
 
-pub fn ping_pong_call_pong_all_scen_steps(world: &mut ScenarioWorld) {
-    ping_pong_call_ping_second_user_scen_steps(world);
-
-    world
-        .current_block()
-        .block_timestamp_millis(TimestampMillis::new(123_781u64));
-
-    ping_pong_call_pong_all_steps_steps(world);
-}
-
-#[test]
-fn ping_pong_call_ping_after_deadline_scen() {
-    let mut world = world();
-    ping_pong_call_ping_after_deadline_scen_steps(&mut world);
-}
-
-pub fn ping_pong_call_ping_after_deadline_scen_steps(world: &mut ScenarioWorld) {
+pub fn ping_pong_call_ping_before_beginning_scen_steps(world: &mut ScenarioWorld) {
     ping_pong_init_scen_steps(world);
 
     world
         .current_block()
-        .block_timestamp_millis(TimestampMillis::new(123_781u64));
+        .block_timestamp_millis(TimestampMillis::new(779u64));
 
     world
         .tx()
@@ -143,7 +123,7 @@ pub fn ping_pong_call_ping_after_deadline_scen_steps(world: &mut ScenarioWorld) 
         .typed(ping_pong_egld_proxy::PingPongEgldProxy)
         .ping(IgnoreValue)
         .payment(Payment::try_new(TestTokenId::EGLD_000000, 0, 500_000_000_000u64).unwrap())
-        .with_result(ExpectError(4, "deadline has passed"))
+        .with_result(ExpectError(4, "smart contract not active yet"))
         .run();
 
     world
@@ -154,24 +134,25 @@ pub fn ping_pong_call_ping_after_deadline_scen_steps(world: &mut ScenarioWorld) 
 }
 
 #[test]
-fn ping_pong_call_pong_scen() {
+fn ping_pong_call_ping_second_user_scen() {
     let mut world = world();
-    ping_pong_call_pong_scen_steps(&mut world);
+    ping_pong_call_ping_second_user_scen_steps(&mut world);
 }
 
-pub fn ping_pong_call_pong_scen_steps(world: &mut ScenarioWorld) {
+pub fn ping_pong_call_ping_second_user_scen_steps(world: &mut ScenarioWorld) {
     ping_pong_call_ping_scen_steps(world);
 
     world
         .current_block()
-        .block_timestamp_millis(TimestampMillis::new(123_781u64));
+        .block_timestamp_millis(TimestampMillis::new(781u64));
 
     world
         .tx()
-        .from(PARTICIPANT1_ADDRESS)
+        .from(PARTICIPANT2_ADDRESS)
         .to(PING_PONG_ADDRESS)
         .typed(ping_pong_egld_proxy::PingPongEgldProxy)
-        .pong()
+        .ping(IgnoreValue)
+        .payment(Payment::try_new(TestTokenId::EGLD_000000, 0, 500_000_000_000u64).unwrap())
         .run();
 
     world
@@ -179,77 +160,13 @@ pub fn ping_pong_call_pong_scen_steps(world: &mut ScenarioWorld) {
         .check_storage("str:activationTimestamp", "780")
         .check_storage("str:deadline", "123,780")
         .check_storage("str:pingAmount", "500,000,000,000")
-        .check_storage("str:userStatus|0x0000001", "2")
-        .check_storage("str:user_address_to_id|address:participant1", "1")
-        .check_storage("str:user_count", "1")
-        .check_storage("str:user_id_to_address|0x0000001", "address:participant1");
-}
-
-pub fn ping_pong_call_pong_all_steps_steps(world: &mut ScenarioWorld) {
-    world
-        .tx()
-        .id("pong-all-complete")
-        .from(PARTICIPANT1_ADDRESS)
-        .to(PING_PONG_ADDRESS)
-        .typed(ping_pong_egld_proxy::PingPongEgldProxy)
-        .pong_all()
-        .returns(ExpectValue(ScenarioValueRaw::new("str:completed")))
-        .run();
-
-    world
-        .check_account(PING_PONG_ADDRESS)
-        .check_storage("str:activationTimestamp", "780")
-        .check_storage("str:deadline", "123,780")
-        .check_storage("str:pingAmount", "500,000,000,000")
-        .check_storage("str:userStatus|0x0000001", "2")
-        .check_storage("str:userStatus|0x0000002", "2")
+        .check_storage("str:userStatus|0x0000001", "1")
+        .check_storage("str:userStatus|0x0000002", "1")
         .check_storage("str:user_address_to_id|address:participant1", "1")
         .check_storage("str:user_address_to_id|address:participant2", "2")
         .check_storage("str:user_count", "2")
         .check_storage("str:user_id_to_address|0x0000001", "address:participant1")
         .check_storage("str:user_id_to_address|0x0000002", "address:participant2");
-}
-
-#[test]
-fn ping_pong_init_scen() {
-    let mut world = world();
-    ping_pong_init_scen_steps(&mut world);
-}
-
-pub fn ping_pong_init_scen_steps(world: &mut ScenarioWorld) {
-    world
-        .account(MY_ADDRESS_ADDRESS)
-        .nonce(0u64)
-        .balance(1_000_000u64);
-    world
-        .account(PARTICIPANT1_ADDRESS)
-        .nonce(0u64)
-        .balance(1_800_000_000_000u64);
-    world
-        .account(PARTICIPANT2_ADDRESS)
-        .nonce(0u64)
-        .balance(2_500_000_000_000u64);
-
-    world
-        .tx()
-        .id("deploy")
-        .from(MY_ADDRESS_ADDRESS)
-        .typed(ping_pong_egld_proxy::PingPongEgldProxy)
-        .init(
-            500_000_000_000u64,
-            DurationMillis::new(123_000u64),
-            ScenarioValueRaw::new("1|u64:780"),
-            IgnoreValue,
-        )
-        .code(PING_PONG_EGLD_CODE_PATH)
-        .new_address(PING_PONG_ADDRESS)
-        .run();
-
-    world
-        .check_account(PING_PONG_ADDRESS)
-        .check_storage("str:activationTimestamp", "780")
-        .check_storage("str:deadline", "123,780")
-        .check_storage("str:pingAmount", "500,000,000,000");
 }
 
 #[test]
@@ -287,6 +204,32 @@ pub fn ping_pong_call_ping_twice_scen_steps(world: &mut ScenarioWorld) {
 }
 
 #[test]
+fn ping_pong_call_ping_wrong_amount_scen() {
+    let mut world = world();
+    ping_pong_call_ping_wrong_amount_scen_steps(&mut world);
+}
+
+pub fn ping_pong_call_ping_wrong_amount_scen_steps(world: &mut ScenarioWorld) {
+    ping_pong_init_scen_steps(world);
+
+    world
+        .tx()
+        .from(PARTICIPANT1_ADDRESS)
+        .to(PING_PONG_ADDRESS)
+        .typed(ping_pong_egld_proxy::PingPongEgldProxy)
+        .ping(IgnoreValue)
+        .payment(Payment::try_new(TestTokenId::EGLD_000000, 0, 450_000_000_000u64).unwrap())
+        .with_result(ExpectError(4, "the payment must match the fixed sum"))
+        .run();
+
+    world
+        .check_account(PING_PONG_ADDRESS)
+        .check_storage("str:activationTimestamp", "780")
+        .check_storage("str:deadline", "123,780")
+        .check_storage("str:pingAmount", "500,000,000,000");
+}
+
+#[test]
 fn ping_pong_call_ping_scen() {
     let mut world = world();
     ping_pong_call_ping_scen_steps(&mut world);
@@ -306,6 +249,113 @@ pub fn ping_pong_call_ping_scen_steps(world: &mut ScenarioWorld) {
         .typed(ping_pong_egld_proxy::PingPongEgldProxy)
         .ping(IgnoreValue)
         .payment(Payment::try_new(TestTokenId::EGLD_000000, 0, 500_000_000_000u64).unwrap())
+        .run();
+
+    world
+        .check_account(PING_PONG_ADDRESS)
+        .check_storage("str:activationTimestamp", "780")
+        .check_storage("str:deadline", "123,780")
+        .check_storage("str:pingAmount", "500,000,000,000")
+        .check_storage("str:userStatus|0x0000001", "1")
+        .check_storage("str:user_address_to_id|address:participant1", "1")
+        .check_storage("str:user_count", "1")
+        .check_storage("str:user_id_to_address|0x0000001", "address:participant1");
+}
+
+#[test]
+fn ping_pong_call_pong_all_after_pong_scen() {
+    let mut world = world();
+    ping_pong_call_pong_all_after_pong_scen_steps(&mut world);
+}
+
+pub fn ping_pong_call_pong_all_after_pong_scen_steps(world: &mut ScenarioWorld) {
+    ping_pong_call_pong_scen_steps(world);
+
+    world
+        .current_block()
+        .block_timestamp_millis(TimestampMillis::new(123_781u64));
+
+    world
+        .tx()
+        .from(PARTICIPANT1_ADDRESS)
+        .to(PING_PONG_ADDRESS)
+        .typed(ping_pong_egld_proxy::PingPongEgldProxy)
+        .pong_all()
+        .returns(ExpectValue(ScenarioValueRaw::new("str:completed")))
+        .run();
+
+    world
+        .check_account(PING_PONG_ADDRESS)
+        .check_storage("str:activationTimestamp", "780")
+        .check_storage("str:deadline", "123,780")
+        .check_storage("str:pingAmount", "500,000,000,000")
+        .check_storage("str:userStatus|0x0000001", "2")
+        .check_storage("str:user_address_to_id|address:participant1", "1")
+        .check_storage("str:user_count", "1")
+        .check_storage("str:user_id_to_address|0x0000001", "address:participant1");
+}
+
+#[test]
+fn ping_pong_call_pong_all_scen() {
+    let mut world = world();
+    ping_pong_call_pong_all_scen_steps(&mut world);
+}
+
+pub fn ping_pong_call_pong_all_scen_steps(world: &mut ScenarioWorld) {
+    ping_pong_call_ping_second_user_scen_steps(world);
+
+    world
+        .current_block()
+        .block_timestamp_millis(TimestampMillis::new(123_781u64));
+
+    ping_pong_call_pong_all_steps_steps(world);
+}
+
+pub fn ping_pong_call_pong_all_steps_steps(world: &mut ScenarioWorld) {
+    world
+        .tx()
+        .id("pong-all-complete")
+        .from(PARTICIPANT1_ADDRESS)
+        .to(PING_PONG_ADDRESS)
+        .typed(ping_pong_egld_proxy::PingPongEgldProxy)
+        .pong_all()
+        .returns(ExpectValue(ScenarioValueRaw::new("str:completed")))
+        .run();
+
+    world
+        .check_account(PING_PONG_ADDRESS)
+        .check_storage("str:activationTimestamp", "780")
+        .check_storage("str:deadline", "123,780")
+        .check_storage("str:pingAmount", "500,000,000,000")
+        .check_storage("str:userStatus|0x0000001", "2")
+        .check_storage("str:userStatus|0x0000002", "2")
+        .check_storage("str:user_address_to_id|address:participant1", "1")
+        .check_storage("str:user_address_to_id|address:participant2", "2")
+        .check_storage("str:user_count", "2")
+        .check_storage("str:user_id_to_address|0x0000001", "address:participant1")
+        .check_storage("str:user_id_to_address|0x0000002", "address:participant2");
+}
+
+#[test]
+fn ping_pong_call_pong_before_deadline_scen() {
+    let mut world = world();
+    ping_pong_call_pong_before_deadline_scen_steps(&mut world);
+}
+
+pub fn ping_pong_call_pong_before_deadline_scen_steps(world: &mut ScenarioWorld) {
+    ping_pong_call_ping_scen_steps(world);
+
+    world
+        .current_block()
+        .block_timestamp_millis(TimestampMillis::new(123_000u64));
+
+    world
+        .tx()
+        .from(PARTICIPANT1_ADDRESS)
+        .to(PING_PONG_ADDRESS)
+        .typed(ping_pong_egld_proxy::PingPongEgldProxy)
+        .pong()
+        .with_result(ExpectError(4, "can\'t withdraw before deadline"))
         .run();
 
     world
@@ -349,75 +399,6 @@ pub fn ping_pong_call_pong_twice_scen_steps(world: &mut ScenarioWorld) {
 }
 
 #[test]
-fn ping_pong_call_ping_second_user_scen() {
-    let mut world = world();
-    ping_pong_call_ping_second_user_scen_steps(&mut world);
-}
-
-pub fn ping_pong_call_ping_second_user_scen_steps(world: &mut ScenarioWorld) {
-    ping_pong_call_ping_scen_steps(world);
-
-    world
-        .current_block()
-        .block_timestamp_millis(TimestampMillis::new(781u64));
-
-    world
-        .tx()
-        .from(PARTICIPANT2_ADDRESS)
-        .to(PING_PONG_ADDRESS)
-        .typed(ping_pong_egld_proxy::PingPongEgldProxy)
-        .ping(IgnoreValue)
-        .payment(Payment::try_new(TestTokenId::EGLD_000000, 0, 500_000_000_000u64).unwrap())
-        .run();
-
-    world
-        .check_account(PING_PONG_ADDRESS)
-        .check_storage("str:activationTimestamp", "780")
-        .check_storage("str:deadline", "123,780")
-        .check_storage("str:pingAmount", "500,000,000,000")
-        .check_storage("str:userStatus|0x0000001", "1")
-        .check_storage("str:userStatus|0x0000002", "1")
-        .check_storage("str:user_address_to_id|address:participant1", "1")
-        .check_storage("str:user_address_to_id|address:participant2", "2")
-        .check_storage("str:user_count", "2")
-        .check_storage("str:user_id_to_address|0x0000001", "address:participant1")
-        .check_storage("str:user_id_to_address|0x0000002", "address:participant2");
-}
-
-#[test]
-fn ping_pong_call_pong_all_after_pong_scen() {
-    let mut world = world();
-    ping_pong_call_pong_all_after_pong_scen_steps(&mut world);
-}
-
-pub fn ping_pong_call_pong_all_after_pong_scen_steps(world: &mut ScenarioWorld) {
-    ping_pong_call_pong_scen_steps(world);
-
-    world
-        .current_block()
-        .block_timestamp_millis(TimestampMillis::new(123_781u64));
-
-    world
-        .tx()
-        .from(PARTICIPANT1_ADDRESS)
-        .to(PING_PONG_ADDRESS)
-        .typed(ping_pong_egld_proxy::PingPongEgldProxy)
-        .pong_all()
-        .returns(ExpectValue(ScenarioValueRaw::new("str:completed")))
-        .run();
-
-    world
-        .check_account(PING_PONG_ADDRESS)
-        .check_storage("str:activationTimestamp", "780")
-        .check_storage("str:deadline", "123,780")
-        .check_storage("str:pingAmount", "500,000,000,000")
-        .check_storage("str:userStatus|0x0000001", "2")
-        .check_storage("str:user_address_to_id|address:participant1", "1")
-        .check_storage("str:user_count", "1")
-        .check_storage("str:user_id_to_address|0x0000001", "address:participant1");
-}
-
-#[test]
 fn ping_pong_call_pong_without_ping_scen() {
     let mut world = world();
     ping_pong_call_pong_without_ping_scen_steps(&mut world);
@@ -447,17 +428,17 @@ pub fn ping_pong_call_pong_without_ping_scen_steps(world: &mut ScenarioWorld) {
 }
 
 #[test]
-fn ping_pong_call_pong_before_deadline_scen() {
+fn ping_pong_call_pong_scen() {
     let mut world = world();
-    ping_pong_call_pong_before_deadline_scen_steps(&mut world);
+    ping_pong_call_pong_scen_steps(&mut world);
 }
 
-pub fn ping_pong_call_pong_before_deadline_scen_steps(world: &mut ScenarioWorld) {
+pub fn ping_pong_call_pong_scen_steps(world: &mut ScenarioWorld) {
     ping_pong_call_ping_scen_steps(world);
 
     world
         .current_block()
-        .block_timestamp_millis(TimestampMillis::new(123_000u64));
+        .block_timestamp_millis(TimestampMillis::new(123_781u64));
 
     world
         .tx()
@@ -465,7 +446,6 @@ pub fn ping_pong_call_pong_before_deadline_scen_steps(world: &mut ScenarioWorld)
         .to(PING_PONG_ADDRESS)
         .typed(ping_pong_egld_proxy::PingPongEgldProxy)
         .pong()
-        .with_result(ExpectError(4, "can\'t withdraw before deadline"))
         .run();
 
     world
@@ -473,30 +453,50 @@ pub fn ping_pong_call_pong_before_deadline_scen_steps(world: &mut ScenarioWorld)
         .check_storage("str:activationTimestamp", "780")
         .check_storage("str:deadline", "123,780")
         .check_storage("str:pingAmount", "500,000,000,000")
-        .check_storage("str:userStatus|0x0000001", "1")
+        .check_storage("str:userStatus|0x0000001", "2")
         .check_storage("str:user_address_to_id|address:participant1", "1")
         .check_storage("str:user_count", "1")
         .check_storage("str:user_id_to_address|0x0000001", "address:participant1");
 }
 
 #[test]
-fn ping_pong_call_get_user_addresses_scen() {
+fn ping_pong_init_scen() {
     let mut world = world();
-    ping_pong_call_get_user_addresses_scen_steps(&mut world);
+    ping_pong_init_scen_steps(&mut world);
 }
 
-pub fn ping_pong_call_get_user_addresses_scen_steps(world: &mut ScenarioWorld) {
-    ping_pong_call_ping_second_user_scen_steps(world);
+pub fn ping_pong_init_scen_steps(world: &mut ScenarioWorld) {
+    world
+        .account(MY_ADDRESS_ADDRESS)
+        .nonce(0u64)
+        .balance(1_000_000u64);
+    world
+        .account(PARTICIPANT1_ADDRESS)
+        .nonce(0u64)
+        .balance(1_800_000_000_000u64);
+    world
+        .account(PARTICIPANT2_ADDRESS)
+        .nonce(0u64)
+        .balance(2_500_000_000_000u64);
 
     world
         .tx()
-        .from(PARTICIPANT1_ADDRESS)
-        .to(PING_PONG_ADDRESS)
+        .id("deploy")
+        .from(MY_ADDRESS_ADDRESS)
         .typed(ping_pong_egld_proxy::PingPongEgldProxy)
-        .get_user_addresses()
-        .returns(ExpectValue(MultiValueVec::from(vec![
-            ScenarioValueRaw::new("address:participant1"),
-            ScenarioValueRaw::new("address:participant2"),
-        ])))
+        .init(
+            500_000_000_000u64,
+            DurationMillis::new(123_000u64),
+            ScenarioValueRaw::new("1|u64:780"),
+            IgnoreValue,
+        )
+        .code(PING_PONG_EGLD_CODE_PATH)
+        .new_address(PING_PONG_ADDRESS)
         .run();
+
+    world
+        .check_account(PING_PONG_ADDRESS)
+        .check_storage("str:activationTimestamp", "780")
+        .check_storage("str:deadline", "123,780")
+        .check_storage("str:pingAmount", "500,000,000,000");
 }
--- framework/meta/run-scen-blackbox.sh
+++ framework/meta/run-scen-blackbox.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Run scen-blackbox for digital-cash
 echo "Running scen-blackbox for ping-pong-egld..."
 cargo run scen-blackbox --overwrite --path ../../contracts/examples/ping-pong-egld
 
