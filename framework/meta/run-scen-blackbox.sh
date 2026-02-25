02a74191a0df1c067be6b47f3a2792158662f459
Andrei Marinica <andrei.marinica@multiversx.com>
2026-02-25 18:45:57 +0200

    sh fix

--- framework/meta/run-scen-blackbox.sh
+++ framework/meta/run-scen-blackbox.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Running scen-blackbox for ping-pong-egld..."
-cargo run scen-blackbox --overwrite --path ../../contracts/examples/ping-pong-egld
+cargo run -- scen-blackbox --overwrite --path ../../contracts/examples/ping-pong-egld
 
 echo "Done!"
